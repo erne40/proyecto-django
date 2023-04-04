3502398e4e4790064d24ef60e9703b862eb9db8a
ernesto <ernestomdelc4@gmail.com>
2023-04-04 13:40:31 -0600

    Clase 18

---
+++ inicio/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+#from proyecto_django.views import mi_vista, mostrar_fecha, saludar, mi_primer_template, prueba_template
+from inicio import views
+
+urlpatterns = [
+    path('', views.mi_vista),
+    path('mostrar-fecha/', views.mostrar_fecha),
+    path('saludar/<str:nombre>/<str:apellido>/', views.saludar),
+    path('mi-primer-template/', views.mi_primer_template),
+    path('prueba-template/', views.prueba_template),
+    path('crear-animal/', views.crear_animal)
+]
---
+++ inicio/views.py
@@ -0,0 +1,70 @@
+from django.shortcuts import render
+
+# Create your views here.
+from datetime import datetime
+from django.http import HttpResponse
+from django.template import Template, Context, loader
+from inicio.models import Animal
+
+def mi_vista(request):
+    print('Pase por aca')
+    return HttpResponse('<h1>Mi primera vista</h1>')
+
+# Versión con HttpResponse:
+""" def mostrar_fecha(request):
+    dt = datetime.now()
+    dt_formateado = dt.strftime("%A %d %B %Y %I:%M")
+    return HttpResponse (f'<p>{dt_formateado}</p>') """
+
+def saludar(request, nombre, apellido):
+    return HttpResponse(f'<h1>Hola {nombre} {apellido}</h1>')
+
+def mi_primer_template(request):   
+    archivo = open(r'C:\Users\ernes\OneDrive\Escritorio\Proyectos\proyecto-django\templates\mi_primer_template.html', 'r')
+    template = Template(archivo.read())
+    archivo.close()
+    contexto = Context()
+    template_renderizado = template.render(contexto)
+    return HttpResponse(template_renderizado)
+
+# Versión con Template:
+def mostrar_fecha(request):
+    dt = datetime.now()
+    dt_formateado = dt.strftime("%A %d %B %Y %I:%M")
+    
+    """ archivo = open(r'mostrar_fecha.html', 'r')
+    template = Template(archivo.read())
+    archivo.close() """
+    template = loader.get_template(r'mostrar_fecha.html')
+    
+    """ contexto = Context({'fecha': dt_formateado})
+    template_renderizado = template.render(contexto) """
+    
+    template_renderizado = template.render({'fecha': dt_formateado})
+     
+    return HttpResponse(template_renderizado)
+
+def prueba_template(request):
+    
+    datos = {
+        'nombre': 'Pepito',
+        'apellido': 'Grillo',
+        'edad': 25,
+        'años': [
+            1995, 2004, 2014, 2017, 2021, 2022
+        ]
+        }
+    
+    template = loader.get_template(r'prueba_template.html')
+    template_renderizado = template.render(datos)
+    return HttpResponse(template_renderizado)
+
+def crear_animal(request):
+    animal = Animal(nombre='Ricardito', edad=3)
+    print(animal.nombre)
+    print(animal.edad)
+    animal.save()
+    datos = {'animal':animal}
+    template = loader.get_template(r'crear_animal.html')
+    template_renderizado = template.render(datos)
+    return HttpResponse(template_renderizado)   
